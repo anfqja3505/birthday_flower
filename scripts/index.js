df926df449e449d6ab2a5efb643466c93d7c382a
anfqja3505 <anfqja3505@gmail.com>
2026-07-16 10:56:21 +0900

    탄생화완성

--- scripts/index.js
+++ scripts/index.js
@@ -3,11 +3,15 @@
 const popup = document.querySelector('.result_popup');
 const popup_bg = document.querySelector('.birthday_flower_bg');
 
-console.log(popup)
-console.log(popup_bg)
+console.log(popup, popup_bg)
 
+//자바스크립트에서 css 쓰려면 작성하는 순서? 객체.style.속성= 값
 popup.style.display = 'none';
 popup_bg.style.display = 'none';
+//자바스크립트에서 display=none으로 숨겼다가 특정이벤트에 따라 다시 보이고 싶을 때
+//무조건 block을 쓰는게 아닌 그 선택자가 원래 가지고 있는 display 속성으로 되돌리기
+// display:flex가 선택자에 원래 있었다면 자바스크립트도 display=flex;
+// display가 선택자에 없었다면? 원래 태그 고유속성에 따라 display=block 또는 display=inline
 
 function popupOpen() {
     popup.style.display = 'flex';
